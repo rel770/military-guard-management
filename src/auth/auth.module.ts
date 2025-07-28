f9e5c177a2f00478c38cb89353cb151444ddee74
rel700 <rel@mail.com>
2025-07-28 16:11:29 +0300

    fix: remove duplicate import of AuthController in auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController]
