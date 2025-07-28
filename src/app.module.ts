39a384ade3c40bc8e214faa180f7f2e7f44cf1c6
rel700 <rel@mail.com>
2025-07-28 16:06:12 +0300

    nest generate module assignments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ShiftsModule } from './shifts/shifts.module';
+import { AssignmentsModule } from './assignments/assignments.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ShiftsModule],
+  imports: [UsersModule, AuthModule, ShiftsModule, AssignmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
