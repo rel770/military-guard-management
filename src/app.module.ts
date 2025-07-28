b8c304afa37d2a4209312203ab538eee6cdec970
rel700 <rel@mail.com>
2025-07-28 16:06:28 +0300

    nest generate module common

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ShiftsModule } from './shifts/shifts.module';
 import { AssignmentsModule } from './assignments/assignments.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ShiftsModule, AssignmentsModule],
+  imports: [UsersModule, AuthModule, ShiftsModule, AssignmentsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
