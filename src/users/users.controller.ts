e379d107b3459df4894fc6bbbbdadeaa18b5e912
rel700 <rel@mail.com>
2025-07-28 17:50:22 +0300

    Implement findAll method in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return {
+      message: 'Mock: Get all users',
+      data: [
+        { id: 1, name: 'John Soldier', role: 'soldier' },
+        { id: 2, name: 'John Commander', role: 'commander' },
+      ],
+    };
+  }
+}
