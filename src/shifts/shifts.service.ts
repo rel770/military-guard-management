75d0644bf5126b60d1b94d5d10b119345a8b5883
rel700 <rel@mail.com>
2025-07-29 12:40:38 +0300

    Define Shift interface in shifts.service.ts

--- src/shifts/shifts.service.ts
+++ src/shifts/shifts.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+interface Shift {
+  id: number;
+  startTime: Date;
+  endTime: Date;
+  location: string;
+  description?: string;
+  createdAt: Date;
+}
+
 @Injectable()
 export class ShiftsService {}
