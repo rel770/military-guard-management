7947348e79c48384907f020961bb9d13a9f824cc
rel700 <rel@mail.com>
2025-07-29 17:54:35 +0300

    Add ShiftsService in the constructor to ShiftsController

--- src/shifts/shifts.controller.ts
+++ src/shifts/shifts.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Param, Post, Body, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ShiftsService } from './shifts.service';
 
 @Controller('shifts')
 @UseGuards(JwtAuthGuard)
 export class ShiftsController {
+  constructor(private readonly shiftsService: ShiftsService) {}
+
   @Get()
   findAll() {
     return {
