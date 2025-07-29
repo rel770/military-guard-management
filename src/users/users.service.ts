c9c875e79a5f1df7dc11d0fb2991f6130d30a534
rel700 <rel@mail.com>
2025-07-29 12:01:05 +0300

    Define User interface in users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Role } from '../common/enums/role.enum';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
+  createdAt: Date;
+}
 
 @Injectable()
 export class UsersService {}
