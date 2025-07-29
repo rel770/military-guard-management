cfe307dea9ddfef0a3d4cc80fde2f873328a27ec
rel700 <rel@mail.com>
2025-07-29 17:12:32 +0300

    Refactor UsersController to include UsersService in the constructor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,10 +2,13 @@ import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { UsersService } from './users.service';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   findAll() {
     return {
