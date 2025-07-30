8357c505ac1eade351789a215dcdd931228d7e44
rel700 <rel@mail.com>
2025-07-30 12:16:02 +0300

    Refactor AssignmentsModule: Import DatabaseModule for database integration

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AssignmentsController } from './assignments.controller';
 import { AssignmentsService } from './assignments.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [AssignmentsController],
   providers: [AssignmentsService]
 })
